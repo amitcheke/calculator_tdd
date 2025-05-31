3165ac1e8419e05cef8274e4466edf40664288a0
amit <amitcheke@hotmail.com>
2025-05-31 17:05:48 +0530

    Adding basic UI component textarea and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Calculator 
-      </header>
+      <header className="App-header">String Calculator</header>
+      <main>
+        <div className="calculator-container">
+          <textarea placeholder="Please enter number string" />
+          <button>Calculate</button>
+        </div>
+      </main>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders text input and button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/calculator/i);
-  expect(linkElement).toBeInTheDocument();
+  const input = screen.getByPlaceholderText(/Please enter number string/);
+  const btn = screen.getByRole("button", { name: /calculate/i });
+  expect(input).toBeInTheDocument();
+  expect(btn).toBeInTheDocument();
 });
